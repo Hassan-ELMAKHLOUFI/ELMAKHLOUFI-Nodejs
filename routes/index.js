81dcaca98642e39b1400f19ab3fe7b20a313d58b
Hassan-ELMAKHLOUFi <hassanelmakhloufi2000@gmail.com>
2021-06-03 18:06:11 +0100

    Etape 17

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,10 @@
 var router = require("express").Router()
 var userRoute = require('./users')(router)
+const fs = require('fs');
+router.get('/',(req,res)=>{res.sendFile('/Users/softedel/Desktop/ELMAKHLOUFI-Nodejs/elmakhloufi/public/index.html')})
+;
+
+
+// const html = fs.readFileSync('public');
+// res.json({html: html.toString(), data: obj});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,39 @@
 var User = require('../repositories/users')
+
 module.exports = function(router){
 	router.route('/users')
 		.get(async function(req, res){
 			user = await User.getAllUsers();
-			//res.send(user);
-      res.render('index',user);
+			res.send(user);
+			//res.render('index',user);
+			
 		})
 		.post(async function(req, res){
 			console.log(req.body);
 			// verification de data
-			var createdUser = await User.addUser(req.body);
+		var createdUser = await User.addUser(req.body);
+		user = await User.getAllUsers();
+		res.send(user);
+     	// res.render('index',user);
+        
 
-			res.send('User added');
+		}).put(async function(req, res) {
+			const datareq = req.body;
+			const data = {
+			  username : req.body.username,
+			  email : req.body.email,
+			  password : req.body.password,
+			  role : req.body.role
+			}
+		
+			await User.updateUser(req.body.id,data) 
+			user = await User.getAllUsers();
+			res.send(user);
+		}).delete(async function (req,res){
+		
+				await User.deleteUser(req.body.id)
+				user = await User.getAllUsers();
+				res.send(user);
 
 		})
 }
